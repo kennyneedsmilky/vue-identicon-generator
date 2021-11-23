93ce7fb68f2fa74cb146a3ad85411697beee8f48
Kenneth Fullbright <kennyneedsmilky@gmail.com>
2021-11-23 12:27:49 -0600

    Added important files.
    Signed-off-by: Kenneth Fullbright <kennyneedsmilky@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import { toSvg } from 'jdenticon';
 
-// For testing.
-import testMessage, {animals} from "./assets/js/test.js";
-Object.defineProperty(Vue.prototype, '$test', { value: testMessage });
-Object.defineProperty(Vue.prototype, '$animals', { value: animals });
 Object.defineProperty(Vue.prototype, '$toSvg', { value: toSvg });
 
 Vue.config.productionTip = false;
